e3d72d70367a8e54d9245402b7b48febab808e80
Nishant <nishant01@gmail.com>
2017-08-03 18:35:29 +0530

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Spree::Core::Engine.add_routes do
-  post '/payumoney', :to => "payumoney#index", :as => :payumoney_proceed
+  get '/payumoney', :to => "payumoney#index", :as => :payumoney_proceed
   post '/payumoney/confirm', :to => "payumoney#confirm", :as => :payumoney_confirm
   post '/payumoney/cancel', :to => "payumoney#cancel", :as => :payumoney_cancel
 end
